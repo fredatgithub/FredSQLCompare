c30811f868e6325798f26b30a6b12dd7a51db0d9
Fred <domaine75@gmail.com>
2020-11-17 12:57:23 +0100

    Initial commit

---
+++ FredSQLCompare/DAL/Connexions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FredSQLCompare.DAL
+{
+  public static class Connexions
+  {
+    public static string GetGeneralConnexionString()
+    {
+      return "Data Source = {serverName}; Initial Catalog = {databaseName}; Persist Security Info = True; User ID = {userName}; Password = {password}";
+    }
+  }
+}
---
+++ FredSQLCompare/Utile/Enumerations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FredSQLCompare.Utile
+{
+  public class Enumerations
+  {
+    public enum SqlObjectType2
+    {
+      Table,
+      StoredProcedure
+    }
+  }
+}
