7df042459cf7281e6490d467a79ecc99ee926793
Fred <domaine75@gmail.com>
2020-11-19 16:21:21 +0100

    Add result line class

---
+++ FredSQLCompare/Model/ResultLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static FredSQLCompare.Utile.Enumerations;
+
+namespace FredSQLCompare.Model
+{
+  public class ResultLine
+  {
+    public SqlObjectTypeSimpleList SourceSqlObjectType { get; set; }
+
+    public DateTime SourceLastModified { get; set; }
+    public string SourceSqlObjectName { get; set; }
+    public bool SqlObjectSelected { get; set; }
+    public string TargetSqlObjectName { get; set; }
+    public DateTime TargetLastModified { get; set; }
+
+    public ResultLine()
+    {
+
+    }
+
+    public ResultLine(SqlObjectTypeSimpleList sqlType, DateTime sourceLastModified, string sourceSqlObjectName, bool sqlObjectSelected, string targetSqlObjectName, DateTime targetLastModified)
+    {
+      SourceSqlObjectType = sqlType;
+      SourceLastModified = sourceLastModified;
+      SourceSqlObjectName = sourceSqlObjectName;
+      SqlObjectSelected = sqlObjectSelected;
+      TargetSqlObjectName = targetSqlObjectName;
+      TargetLastModified = targetLastModified;
+    }
+  }
+}
