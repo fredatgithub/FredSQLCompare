6621a9d1688006e4d23fe5f198e7bcfb4cbf4eec
Fred <domaine75@gmail.com>
2020-11-21 07:22:42 +0100

    Add NuGet packages

--- FredSQLCompare/FormMain.cs
+++ FredSQLCompare/FormMain.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using FredSQLCompare.Properties;
 using FredSQLCompare.View;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace FredSQLCompare
 {
@@ -20,6 +23,8 @@ namespace FredSQLCompare
         //_configurationOptions = frmDbConnexion.ConfigurationOptions2;
       }
 
+      GetWindowValue();
+      DisplayTitle();
     }
 
     private void QuitterToolStripMenuItem_Click(object sender, EventArgs e)
@@ -35,6 +40,36 @@ namespace FredSQLCompare
     private void ButtonDeploy_Click(object sender, EventArgs e)
     {
 
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      // You can place this code on File.Quit Menu method if you have one or here (window closing method)
+      SaveWindowValue();
+    }
+
+    private void SaveWindowValue()
+    {
+      Settings.Default.WindowHeight = Height;
+      Settings.Default.WindowWidth = Width;
+      Settings.Default.WindowLeft = Left;
+      Settings.Default.WindowTop = Top;
+      Settings.Default.Save();
+    }
+
+    private void GetWindowValue()
+    {
+      Width = Settings.Default.WindowWidth;
+      Height = Settings.Default.WindowHeight;
+      Top = Settings.Default.WindowTop < 0 ? 0 : Settings.Default.WindowTop;
+      Left = Settings.Default.WindowLeft < 0 ? 0 : Settings.Default.WindowLeft;
+    }
+
+    private void DisplayTitle()
+    {
+      Assembly assembly = Assembly.GetExecutingAssembly();
+      FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+      Text += string.Format(" V{0}.{1}.{2}.{3}", fvi.FileMajorPart, fvi.FileMinorPart, fvi.FileBuildPart, fvi.FilePrivatePart);
     }
   }
 }
