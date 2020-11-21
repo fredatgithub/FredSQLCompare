1d4515879ffe9a8f102452368d2fc452272c5e9b
Fred <domaine75@gmail.com>
2020-11-21 07:10:22 +0100

    Add unit test project

---
+++ UnitTestFredSQLCompare/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFredSQLCompare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFredSQLCompare")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6b449ec-f2c0-4915-984b-a7e21a2eea07")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestFredSQLCompare/UnitTestString.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tools;
+
+namespace UnitTestFredSQLCompare
+{
+  [TestClass]
+  public class UnitTestString
+  {
+    [TestMethod]
+    public void TestMethod_Tabulate_1()
+    {
+      ushort source = 1;
+      string expected = " ";
+      string result = Punctuation.Tabulate(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_CreateSentence_No_Space()
+    {
+      string source = "";
+      string expected = "";
+      string result = Punctuation.CreateSentence(source);
+      Assert.AreEqual(result, expected);
+    }
+
+
+    [TestMethod]
+    public void TestMethod_CreateSentence_One_character()
+    {
+      string source = "a";
+      string expected = "a";
+      string result = Punctuation.CreateSentence(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_CreateSentence_One_Sentence()
+    {
+      string source = "a long long time ago";
+      string expected = "a long long time ago";
+      string result = Punctuation.CreateSentence(source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_CreateSentence_several_Characters()
+    {
+      string source = "a long";
+      string source2 = " long ";
+      string source3 = "time ago";
+      string expected = "a long long time ago";
+      string result = Punctuation.CreateSentence(source, source2, source3);
+      Assert.AreEqual(result, expected);
+    }
+  }
+}
