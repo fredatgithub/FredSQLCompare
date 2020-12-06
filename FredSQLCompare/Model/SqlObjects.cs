971537a73cf98e18e093e0814b2aa72f54d2b84e
Fred <domaine75@gmail.com>
2020-12-06 18:52:15 +0100

    Add sql objects model class

---
+++ FredSQLCompare/Model/SqlObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace FredSQLCompare.Model
+{
+  public class SqlObjects
+  {
+    public List<string> ListOfTables { get; set; }
+    public List<string> ListOfStoredProcedures { get; set; }
+    public List<string> ListOfViews { get; set; }
+    public List<string> ListOfConstraints { get; set; }
+
+    public SqlObjects()
+    {
+      ListOfTables = new List<string>();
+      ListOfStoredProcedures = new List<string>();
+      ListOfViews = new List<string>();
+      ListOfConstraints = new List<string>();
+    }
+  }
+}
